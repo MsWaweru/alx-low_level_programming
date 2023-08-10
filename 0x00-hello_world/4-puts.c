be2d673cd62820ac48e00bf18b00080381674b6f
MsWaweru <waweruann92@gmail.com>
2023-08-10 15:01:18 -0700

    Programming is like building a multilingual puzzle

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - C Program that prints a given line followed by a new line
+ *
+ * Return: zero on success
+ *
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
