0b826600827bdd8ceb031513934fec4cf20f4709
MsWaweru <waweruann92@gmail.com>
2023-09-01 22:57:57 -0700

    static library libmy.a containing all the previosly coded functions

---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - computing absolute values of integers
+ * @d: function parameter
+ * Return: absolute value of int
+ */
+int _abs(int d)
+{
+	if (d >= 0)
+	{
+		return (d);
+	}
+	return (-d);
+}
