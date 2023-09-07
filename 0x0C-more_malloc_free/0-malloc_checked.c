551e21a29230e6b8bb1854131a7b57b2a0a898c2
MsWaweru <waweruann92@gmail.com>
2023-09-07 13:54:48 -0700

    0-malloc_checked.c

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * *malloc_checked - Main entry
+ * @b: input
+ *
+ * Return: 0
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *p;
+
+	p = malloc(b);
+	if (p == NULL)
+		exit(98);
+
+	return (p);
+}
