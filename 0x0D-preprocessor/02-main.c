a938413011c429780775e9ced64b7fadb21176b6
MsWaweru <waweruann92@gmail.com>
2023-09-12 14:17:46 -0700

    2-main.c

--- 0x0D-preprocessor/02-main.c
+++ 0x0D-preprocessor/02-main.c
@@ -3,10 +3,10 @@
 /**
  * main - prints the name of the file
  *
- * Return: Always 0 (success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	printf("%s/n", __FILE__);
+	printf("%s\n", __FILE__);
 	return (0);
 }
