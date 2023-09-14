fdc847ce320467f75a19fca11de0e159d2edb726
MsWaweru <waweruann92@gmail.com>
2023-09-14 07:37:37 -0700

    If you spend too much time thinking about a thing, you'll never get it done

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,8 +1,8 @@
 #include "function_pointers.h"
 #include <stdio.h>
 /**
- * arrray_iterator - prints each array elem on a newl
- * @arrray: array
+ * array_iterator - prints each array elem on a newl
+ * @array: array
  * @size: how many elem to print
  * @action: pointer to print in regular or hex
  * Return: void
